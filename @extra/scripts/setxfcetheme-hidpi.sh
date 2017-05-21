1e9688b077f14e900e0f6ddc1792de306ee7942c
ssf <punx665@gmail.com>
2017-05-21 19:27:31 +0200

    So 21. Mai 19:27:31 CEST 2017

--- @extra/scripts/setxfcetheme-hidpi.sh
+++ @extra/scripts/setxfcetheme-hidpi.sh
@@ -34,6 +34,11 @@ else
 	printf "\nAlso don't forget to add GDK_SCALE=2 to your environment!\n"
 fi
 if type yad >/dev/null 2>&1; then
+	yad --title "Qt5 HiDPI" --text="Add QT_SCALE_FACTOR=2 to your environment for Qt5 support" --image="dialog-information" --button="gtk-ok:0"
+else
+	printf "\nAdd QT_SCALE_FACTOR=2 to your environment for Qt5 support\n"
+fi
+if type yad >/dev/null 2>&1; then
 	printf 'xfconf-query -c xsettings -p /Gtk/IconSizes -s "gtk-menu=32,32:gtk-small-toolbar=48,48:gtk-large-toolbar=48,48:gtk-dnd=96,96:gtk-button=48,48:gtk-dialog=96,96:gtk-panel=48,48:panel-applications-menu=48,48:panel-tasklist-menu=64,64:panel-menu=48,48:panel-directory-menu=48,48:panel-launcher-menu=48,48:panel-window-menu=48,48:panel-menu-bar=48,48:ev-icon-size-annot-window=32,32:webkit-media-button-size=48,48"' >/tmp/hidpixfconf.phlat
 
 	yad --text-info --title "GTK2 HiDPI Icons" --text='You might need to double the GTK2 icon size eg:' --filename="/tmp/hidpixfconf.phlat" --image="dialog-information" --button="gtk-ok:0"
